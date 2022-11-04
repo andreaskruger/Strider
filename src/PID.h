f6a6cd07f123d83d4b5deadb3dd8b1846d929444
Andreas Axén <andreas.axen.kruger@gmail.com>
2022-11-04 08:16:49 +0100

    Added QTR for positionreadings.

--- src/PID.h
+++ src/PID.h
@@ -1,5 +1,7 @@
 #include<Arduino.h>
 
+#define basePosition 2500
+
 double Kp = 0.5;  
 double Kd = 2;
 double ki = 0;
@@ -10,12 +12,14 @@ double eTime = 0;
 double cum = 0;
 
 int PID_speed(int position){
-    int error = position - 2500;
+    int error = position - basePosition;
     currentTime = millis();
     eTime = currentTime - lastTime;
     cum =cum + error*eTime;
     int motorSpeed = Kp * error + Kd * ((error - lastError)/(eTime))+ ki*(cum);
     lastError = error;
     lastTime = currentTime;
+    if(motorSpeed < 0){motorSpeed = -1*motorSpeed;}
+    if(motorSpeed >255){motorSpeed = 255;}
     return motorSpeed;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,16 @@
 #include <Arduino.h>
 #include <Sparkfun_TB6612.h>
+#include <QTRSensors.h>
+#include <PID.h>
 
 
-#define AIN1 2
-#define BIN1 7
-#define AIN2 4
-#define BIN2 8
-#define PWMA 3
-#define PWMB 6
-#define STBY 9
+#define AIN1 4
+#define BIN1 2
+#define AIN2 5
+#define BIN2 0
+#define PWMA 6
+#define PWMB 1
+#define STBY 3
 
 #define MaxSpeed 255
 #define BaseSpeed 255 
@@ -22,33 +24,61 @@ int direction;
 Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
 Motor motor2 = Motor(BIN1, BIN2, PWMB, offsetB, STBY);
 
+QTRSensors qtr;
+
+const uint8_t SensorCount = 4;
+uint16_t sensorValues[SensorCount];
+
+
 void move(int motor, int speed, int direction){
-   digitalWrite(STBY, HIGH); 
-   boolean inPin1=HIGH;
-   boolean inPin2=LOW;
-  
-  if(direction == 1){
-    inPin1 = HIGH;
-    inPin2 = LOW;
-  }  
-  if(direction == 0){
-    inPin1 = LOW;
-    inPin2 = HIGH;
+  if(motor == 1){
+    motor1.drive(speed,10);
   }
-   if(motor == 0){
-      digitalWrite(AIN1, inPin1);
-      digitalWrite(AIN2, inPin2);
-      analogWrite(PWMA, speed);
-   }
-   if(motor == 1){
-      digitalWrite(BIN1, inPin2);
-      digitalWrite(BIN2, inPin1);
-      analogWrite(PWMB, speed);
-   }
+  if(motor == 2){
+    motor2.drive(speed,10);
+  }
+}
+
+void calibrateQTR(){
+  // configure the sensors
+  qtr.setTypeRC();
+  qtr.setSensorPins((const uint8_t[]){7, 8, 9, 10}, SensorCount);
+  delay(500);
+
+  // 2.5 ms RC read timeout (default) * 10 reads per calibrate() call
+  // = ~25 ms per calibrate() call.
+  // Call calibrate() 400 times to make calibration take about 10 seconds.
+  for (uint16_t i = 0; i < 400; i++)
+  {
+    qtr.calibrate();
+  }
+  Serial.println("Calibration done, printing calibration values(Minimum): ");
+
+  // print the calibration minimum values measured when emitters were on
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(qtr.calibrationOn.minimum[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+  Serial.println("Calibration done, printing calibration values(Maximum): ");
+  // print the calibration maximum values measured when emitters were on
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(qtr.calibrationOn.maximum[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+  Serial.println();
+  delay(1000);
+}
+int readQTR(){
+  
 }
 
 void setup() {
   //Define motor pins
+  Serial.begin(115200);
   pinMode(AIN1, OUTPUT);
   pinMode(AIN2, OUTPUT);
   pinMode(PWMA, OUTPUT);
@@ -56,12 +86,21 @@ void setup() {
   pinMode(BIN2, OUTPUT);
   pinMode(PWMB, OUTPUT);
   pinMode(STBY, OUTPUT);
+  digitalWrite(STBY, HIGH); 
+  Serial.println("Starting calibration: ");
+  delay(1000);
+  calibrateQTR();
+  Serial.println("Starting loop: ");
 }
 
 void loop() {
-  position = 1;
-  move(0,255,1);
+  
+  position = qtr.readLineBlack(sensorValues);
+  float speed = PID_speed(position);
+  Serial.print("Speed: ");
+  Serial.println(speed);
+  //if(position > 0){ move(0,255-speed,1); }else{ move(1,255-speed,1);};
   move(1,255,1);
-  speed = PID_speed(position);
-  if(position > 0 :move(1,255-speed,1) ? move(2,255-speed,1));
+  move(2,255,1);
+  delay(25);
 }
